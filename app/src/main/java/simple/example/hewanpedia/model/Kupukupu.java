7f8c969ab4672960bf0228a2d2a5ca07676c174f
Respiani14 <Lailihusniati@gmail.com>
2022-01-15 19:48:59 +0700

    first commit

---
+++ app/src/main/java/simple/example/hewanpedia/model/Kupukupu.java
@@ -0,0 +1,10 @@
+package simple.example.hewanpedia.model;
+
+
+public class Kupukupu extends Hewan {
+
+    public Kupukupu(String ras, String asal, String deskripsi, int drawableRes) {
+        super("kupukupu",ras,asal,deskripsi,drawableRes);
+    }
+
+}
